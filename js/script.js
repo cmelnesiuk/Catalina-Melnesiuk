f000a3ff30b13265ea5f9339704d6a32f04be161
Catalina Melnesiuk <catalinamelnesiuk@gmail.com>
2021-05-21 01:13:12 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,10 +17,39 @@ window.onload = function (){
 	$('body').removeClass('hidden');
  }
 
- 
+
 $(document).ready(function(){
     $("#about").click(function(){
         $(".text-about").slideToggle();
     });
 });
 
+function enviar(){
+    
+    function DatosPersona (nombre,correo){
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+    function DatosMsj (asunto, mensaje){
+        this.asunto = asunto;
+        this.mensaje = mensaje; 
+    }
+
+var nombreP1 = document.getElementById("nombre").value
+var correoP1 = document.getElementById("correo").value
+var asuntoP1 = document.getElementById("asunto").value
+var mensajeP1 = document.getElementById("mensaje").value
+//if(nombreP1=="" || correoP1=="" || asuntoP1=="" || mensajeP1==""){
+//    alert("Debe completar todos los datos del formulario")
+//}
+if(nombreP1!=="" && correoP1!=="" && asuntoP1!=="" && mensajeP1!==""){
+
+    var DatosPersona = new DatosPersona(nombreP1, correoP1)
+    var DatosMsj = new DatosMsj(asuntoP1, mensajeP1)
+    var DatosCompletos = [DatosPersona,DatosMsj]
+    localStorage.setItem("Datos de "+nombreP1+":", JSON.stringify(DatosCompletos))
+    console.log(DatosCompletos)
+
+}
+}
+document.getElementById("btn1").addEventListener("click", enviar)
